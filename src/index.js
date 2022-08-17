edebcec6fca8206c0467db1748e53fa9dcfd7d3f
Adrian Khaskin <haskin.dev@gmail.com>
2022-08-17 18:39:52 -0400

    finished implementation

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
-"use strict";
-const button = document.querySelector("button");
-function prepend(numSpaces, inputString) {
-    return inputString.padStart(inputString.length + +numSpaces, " ");
+var button = document.querySelector("button");
+/**
+ * Array where this first element is a number representing the size
+ * of the prepended space and the rest of the values are
+ * input strings
+ * @param spaceAndInputs
+ * @returns
+ */
+function prepend(spaceAndInputs) {
+    // Should have at least two values
+    if (spaceAndInputs.length < 2 || typeof +spaceAndInputs[0] != "number")
+        return [];
+    var numberOfSpaces = +spaceAndInputs[0];
+    var space = "";
+    while (numberOfSpaces-- != 0) {
+        space += " ";
+    }
+    var result = [];
+    for (var i = 1; i < spaceAndInputs.length; i++) {
+        if (typeof spaceAndInputs[i] != "string")
+            continue;
+        result.push(space + spaceAndInputs[i]);
+    }
+    return result;
 }
 button.addEventListener("click", function (e) {
     e.preventDefault();
     console.log("Creating Prepend String");
-    const inputString = document.getElementById("input_string");
-    const inputNumberOfSpaces = document.getElementById("number_of_spaces");
-    const outputSpan = document.getElementById("output_string");
-    console.log(`Input string: ${inputString.value}`);
-    console.log(`Number of spaces: ${inputNumberOfSpaces.value}`);
-    let outputString = prepend(inputNumberOfSpaces.value, inputString.value);
-    console.log(`Output : ${outputString}`);
-    outputSpan.textContent = outputString;
+    var inputs = document.getElementById("input_string");
+    // const inputNumberOfSpaces: HTMLInputElement = document.getElementById("number_of_spaces") as HTMLInputElement;
+    var outputSpan = document.getElementById("output_string");
+    console.log("Inputs : ".concat(inputs.value));
+    // console.log(`Number of spaces: ${inputNumberOfSpaces.value}`);
+    var outputString = prepend(inputs.value.split(",").map(function (input) { return input.trim(); }));
+    console.log("Output : ".concat(outputString));
+    outputSpan.textContent = outputString.join(",");
 });
--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,40 @@
 const button: HTMLButtonElement = document.querySelector("button") as HTMLButtonElement;
 /**
- * Prepends the space based on the array of numbspac
- * @param numSpaces 
- * @param inputStrings 
+ * Array where this first element is a number representing the size
+ * of the prepended space and the rest of the values are 
+ * input strings 
+ * @param spaceAndInputs 
  * @returns 
  */
-function prepend<T>(numSpaces: T[], inputStrings: T[]): string[] {
-    if (numSpaces.length != inputStrings.length)
+function prepend<T>(spaceAndInputs: T[]): string[] {
+    // Should have at least two values
+    if (spaceAndInputs.length < 2 || typeof +spaceAndInputs[0] != "number")
         return [];
+    let numberOfSpaces = +spaceAndInputs[0];
+    let space: string = "";
+    while (numberOfSpaces-- != 0) {
+        space += " ";
+    }
     let result: string[] = [];
-    for (let i: number = 0; i < inputStrings.length; i++) {
-        let inputString: string = inputStrings[i] + "";
-        result.push(inputString.padStart(inputString.length + +numSpaces[i], " "));
+    for (let i: number = 1; i < spaceAndInputs.length; i++) {
+        if (typeof spaceAndInputs[i] != "string")
+            continue;
+        result.push(space + spaceAndInputs[i]);
     }
     return result;
 }
-// function prepend(numSpaces: number | string, inputString: string): string {
-//     return inputString.padStart(inputString.length + +numSpaces, " ");
-// }
-button!.addEventListener("click", function (e) {
+
+button.addEventListener("click", function (e) {
     e.preventDefault();
     console.log("Creating Prepend String");
-    const inputString: HTMLInputElement = document.getElementById("input_string") as HTMLInputElement;
-    const inputNumberOfSpaces: HTMLInputElement = document.getElementById("number_of_spaces") as HTMLInputElement;
+    const inputs: HTMLInputElement = document.getElementById("input_string") as HTMLInputElement;
+    // const inputNumberOfSpaces: HTMLInputElement = document.getElementById("number_of_spaces") as HTMLInputElement;
     const outputSpan: HTMLSpanElement = document.getElementById("output_string") as HTMLSpanElement;
-    console.log(`Input string: ${inputString.value}`);
-    console.log(`Number of spaces: ${inputNumberOfSpaces.value}`);
-    let outputString = prepend(inputNumberOfSpaces!.value, inputString!.value)
+    console.log(`Inputs : ${inputs.value}`);
+    // console.log(`Number of spaces: ${inputNumberOfSpaces.value}`);
+    let outputString = prepend(inputs.value.split(",").map(input => input.trim()));
     console.log(`Output : ${outputString}`);
-    outputSpan.textContent = outputString;
+    outputSpan.textContent = outputString.join(",");
 });
 
 
